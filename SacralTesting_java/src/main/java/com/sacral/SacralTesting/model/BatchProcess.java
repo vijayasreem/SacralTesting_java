d258654b466fddaa1574f2515e2dd827dc707e71
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-05-23 11:44:06 +0530

    initial commit

---
+++ SacralTesting_java/src/main/java/com/sacral/SacralTesting/model/BatchProcess.java
@@ -0,0 +1,62 @@
+package com.sacral.SacralTesting.model;
+
+import Annexure;
+
+public class BatchProcess {
+    
+    // Bulk Claims Settlement
+    public void acceptBulkClaimsSettlementRequests(Annexure annexure) {
+        // code
+    }
+    
+    public void validateInputFileFormat(Annexure annexure) {
+        // code
+    }
+    
+    public void processClaimsSettlementRequests(Annexure annexure) {
+        // code
+    }
+    
+    public void generateClaimsSettlementReport() {
+        // code
+    }
+    
+    // Bulk Data Upload for New Business and Renewal
+    public void allowBulkDataUpload(Annexure annexure) {
+        // code
+    }
+    
+    public void validateDataUploadFileFormat(Annexure annexure) {
+        // code
+    }
+    
+    public void createNewBusinessRecords(Annexure annexure) {
+        // code
+    }
+    
+    public void updateRelevantFields(Annexure annexure) {
+        // code
+    }
+    
+    public void generateDataUploadReport() {
+        // code
+    }
+    
+    // Bulk Data Movement within LIC (Transfer in –Out Cases / Conversion)
+    public void allowBulkDataMovement(Annexure annexure) {
+        // code
+    }
+    
+    public void validateDataMovementFileFormat(Annexure annexure) {
+        // code
+    }
+    
+    public void updateRelevantFieldsInSystem(Annexure annexure) {
+        // code
+    }
+    
+    public void generateDataMovementReport() {
+        // code
+    }
+
+}
